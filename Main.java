10408faa9ee5a62d499668de1c2bea30a59c3a6e
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2020-12-28 20:13:14 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+class Arithmetic
+{
+	public static void main(String args[]) {
+	    int a=2,b=6,c;
+	    c=a*b;
+		System.out.println("Product is"+c);
+		c=a+b;
+		System.out.println("Sum is"+c);
+		c=a-b;
+		System.out.println("Difference is"+c);
+		c=b/a;
+		System.out.println("Division is"+c);
+	}
+}
+
