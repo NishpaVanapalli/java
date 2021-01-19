2b0190aab4758fa80c9472778aeb8600e877b69e
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-01-19 19:11:40 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,11 @@
+class A
+{
+public static void main(String args[])
+{
+int a,b;
+a=Integer.parseInt(args[0]);
+b=Integer.parseInt(args[1]);
+int c=Integer.parseInt(args[2]);
+System.out.println(a+"\n"+b+"\t"+c);
+}
+}
