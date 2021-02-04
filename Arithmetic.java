fa005c0e4e464c5ff85efd36c84960213afb7344
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-02-04 20:09:32 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+class Arithmetic
+{
+public static void main(String args[])
+{
+int a=10,b=20,c;
+c=a+b;
+System.out.println("sum of a and b is "+c);
+c=b-a;
+System.out.println("difference of b and a is "+c);
+c=a*b;
+System.out.println("product of a and b is "+c);
+c=b/a;
+System.out.println("division of b with a gives "+c);
+}
+}
