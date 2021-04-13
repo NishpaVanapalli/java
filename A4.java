3e5d3579abbd595548bb4ee91349e46be626498b
Nishpa <71434369+NishpaVanapalli@users.noreply.github.com>
2021-04-13 16:56:58 +0530

    Add files via upload

---
+++ A4.java
@@ -0,0 +1,15 @@
+class A4
+{
+int b;
+A4()
+{
+this.b=1000;
+}
+public static void main(String args[])
+{
+A4 a1=new A4();
+System.out.println(a1.b);
+A4 a2=new A4();
+System.out.println(a2.b);
+}
+}
